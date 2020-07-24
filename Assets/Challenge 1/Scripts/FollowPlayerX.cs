eb2361820597066dce745d00bd471d2f2dcfc156
Cory Lawrence <35942282+clawrence199@users.noreply.github.com>
2020-07-23 20:55:44 -0500

    Initial Commit

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject camPos;
+    public GameObject plane;
+    public GameObject lookAt;
+    private Vector3 offset = new Vector3(0, 0, -10);
+    public Transform target; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = lookAt.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.SetPositionAndRotation(camPos.transform.position, plane.transform.rotation);
+       // transform.LookAt(target);
+       /* transform.position = plane.transform.position + offset;
+        transform.rotation = plane.transform.rotation;*/
+    }
+}
